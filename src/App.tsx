c658306fd3ed8ae1d5dd0e1f89c67f66996e1fff
lucasdellasala <lucasdellasalamarkovic@gmail.com>
2022-03-12 20:53:19 -0300

    Adding title, input and react.-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import { config } from './config/config';
+import SearchBar from './components/searchBar/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>{config.app_title}</title>
+      </Helmet>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{config.app_title}</h1>
+        <SearchBar/>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+    app_title: 'Videoteca'
+}
